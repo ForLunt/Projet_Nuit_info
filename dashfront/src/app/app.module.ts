9ea3f6e01cf606cc6e8399b0bd3f5236842f9009
Morgane Pechon <97598276+ForLunt@users.noreply.github.com>
2023-12-07 20:41:16 +0000

    mise en place slideBar

--- dashfront/src/app/app.module.ts
+++ dashfront/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SlideBardComponent } from './components/slide-bard/slide-bard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideBardComponent
   ],
   imports: [
     BrowserModule
