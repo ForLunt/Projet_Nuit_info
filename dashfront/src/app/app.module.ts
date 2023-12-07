906685464fe161dec68c07a5c0d4ffa46320279f
antomuz <antomuz19@gmail.com>
2023-12-07 21:41:13 +0100

    correction lien component

--- dashfront/src/app/app.module.ts
+++ dashfront/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PageAcceuilComponent } from './components/components/page-acceuil/page-acceuil.component';
+import { PageAcceuilComponent } from './components/page-acceuil/page-acceuil.component';
 
 @NgModule({
   declarations: [
