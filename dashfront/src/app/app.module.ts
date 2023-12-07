215263c4f8ea340576437ad7c2ebe55bb7ccc471
antomuz <antomuz19@gmail.com>
2023-12-07 21:38:12 +0100

    ajout component page acceuil

--- dashfront/src/app/app.module.ts
+++ dashfront/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageAcceuilComponent } from './components/components/page-acceuil/page-acceuil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageAcceuilComponent
   ],
   imports: [
     BrowserModule
