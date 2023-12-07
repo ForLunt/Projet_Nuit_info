0710211145dd77aca2ee8a9b22bbcc9c2b3cc070
LAPTOP-BA6UDKDD\Utilisateur <alexrioux44@gmail.com>
2023-12-07 22:27:22 +0100

    réglage conflit

--- dashfront/src/app/app.module.ts
+++ dashfront/src/app/app.module.ts
@@ -1,29 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { PageInformationComponent } from './components/page-information/page-information.component';
-=======
-=======
 import { SlideBardComponent } from './components/slide-bard/slide-bard.component';
->>>>>>> 06a8360b8b366d78e43c84668bdb0304733eec65
 import { PageAcceuilComponent } from './components/page-acceuil/page-acceuil.component';
->>>>>>> 7bc08a4c87b6cd5f9a80e7f70c45d2df302b7657
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    PageInformationComponent
-=======
-=======
-    SlideBardComponent
->>>>>>> 06a8360b8b366d78e43c84668bdb0304733eec65
+    PageInformationComponent,
+    SlideBardComponent,
     PageAcceuilComponent
->>>>>>> 7bc08a4c87b6cd5f9a80e7f70c45d2df302b7657
   ],
   imports: [
     BrowserModule
