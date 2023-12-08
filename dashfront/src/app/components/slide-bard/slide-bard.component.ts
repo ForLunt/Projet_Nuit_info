9b57fe5045178e40fcefbf1568289cd6f3922d71
Morgane Pechon <97598276+ForLunt@users.noreply.github.com>
2023-12-08 06:00:22 +0000

    envoye bouton

--- dashfront/src/app/components/slide-bard/slide-bard.component.ts
+++ dashfront/src/app/components/slide-bard/slide-bard.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-SlidBard',
   templateUrl: './slide-bard.component.html',
   styleUrls: ['./slide-bard.component.css']
 })
-export class SlideBardComponent {
-
+export class SlideBardComponent implements OnInit {
+  lien: string=" ";
+  public ngOnInit(): void {
+    this.lien="https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal";
+  }
+  reload(){
+    location.reload();
+  }
 }
